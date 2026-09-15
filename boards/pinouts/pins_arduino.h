750e0124e18d3a6c6d855641aa6c54821d23dd27
mattykmooney1988-coder <mattykmooney1988@gmail.com>
2026-09-16 00:07:04 +0100

    Checkpoint from VS Code for cloud agent session (#2656)
    Co-authored-by: Pirata <104320209+bmorcelli@users.noreply.github.com>

--- boards/pinouts/pins_arduino.h
+++ boards/pinouts/pins_arduino.h
@@ -60,6 +60,8 @@
 #include "../ESP32-C5/pins_arduino.h"
 #elif NM_CYD_ESP32C5
 #include "../nm-cyd-c5/pins_arduino.h"
+#elif FREENOVE_FNK0103
+#include "../freenove-fnk0103/pins_arduino.h"
 #elif CONFIG_IDF_TARGET_ESP32P4
 #include "../m5stack-tab5/pins_arduino.h"
 #endif
