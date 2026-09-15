1730a9f245d0c0f525286a6fc1e2a534a7c36aff
uchiha-madara-02 <namnguyenhoang19042008@gmail.com>
2026-09-16 06:31:48 +0700

    port: ESP32-S3 Supermini with ST7789 (#2349)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add include for MADARA_BOARD pinout
    
    * Uncomment esp32-c5 and add S3_supermini_madara
    
    * Add S3_supermini_madara environment to workflow
    
    * Add S3_supermini_madara environment to workflow
    
    ---------
    
    Co-authored-by: uchiha-madara-02 <phamthithanhuyen08@gmail.com>
    Co-authored-by: Pirata <104320209+bmorcelli@users.noreply.github.com>

--- boards/pinouts/pins_arduino.h
+++ boards/pinouts/pins_arduino.h
@@ -58,6 +58,8 @@
 #include "../ESP32-C5-tft/pins_arduino.h"
 #elif ESP32C5_DEVKITC_1
 #include "../ESP32-C5/pins_arduino.h"
+#elif MADARA_BOARD
+#include "../S3_supermini_madara/pins_arduino.h"
 #elif WAVESHARE_C5_TFT
 #include "../waveshare-c5-tft/pins_arduino.h"
 #elif NM_CYD_ESP32C5
